23996e9806f25789cd501353a0a0b0049d86ff9d
SAMUEL <SAMUEL@LAPTOP-RQ08L5ED>
2023-08-30 22:30:49 -0500

    Ejercicio Propuesto No14

---
+++ src/EjercicioPropuesto14.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class EjercicioPropuesto14 {
+
+	public static void main(String[] args) {
+		double x;
+		
+		Scanner entrada = new Scanner(System.in);
+		x = entrada.nextDouble();
+		
+		System.out.println("El valor al cuadrado es: "+ x*x);
+		System.out.print("El valor al cubo es: "+ x*x*x);
+		
+
+	}
+
+}
